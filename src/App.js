f410551b79c6a59293fd7c7c8c4d6a6b20a7298a
Marco Friaz <marfriaz@gmail.com>
2020-06-14 12:32:03 -0700

    Function Component added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import List from "./List";
+import "./App.css";
+import STORE from "./STORE";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main className="App">
+      <header class="App-header">
+        <h1>Trelloyes!</h1>
       </header>
-    </div>
+      <div class="App-list">
+        {STORE.lists.map((list) => (
+          <List
+            key={list.id}
+            header={list.header}
+            cards={list.cardIds.map((id) => STORE.allCards[id])}
+          />
+        ))}
+      </div>
+    </main>
   );
 }
 
 export default App;
+
+class AppClass extends Component {
+  static defaultProps = {
+    store: {
+      lists: [],
+      allCards: {},
+    },
+  };
+
+  render() {
+    const { store } = this.props;
+    return (
+      <main className="App">
+        <header className="App-header">
+          <h1>Trelloyes!</h1>
+        </header>
+        <div className="App-list">
+          {store.lists.map((list) => (
+            <List
+              key={list.id}
+              header={list.header}
+              cards={list.cardIds.map((id) => store.allCards[id])}
+            />
+          ))}
+        </div>
+      </main>
+    );
+  }
+}
